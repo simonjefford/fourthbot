d513f7eb9cacbcfce2e5b0101e7d0f8a36a9cc0a
Simon Jefford <simon.jefford@fourth.com>
2017-06-19 18:17:29 +0100

    extract Initializer

--- slack/init.go
+++ slack/init.go
@@ -5,6 +5,10 @@ import (
 	"go4.org/jsonconfig"
 )
 
+// An Initializer is a function thaat takes a piece of json config and
+// returns a responder or an error
+type Initializer func(jsonconfig.Obj) (fourthbot.RegisteringResponder, error)
+
 // InitializerTable is a type that stores initialization functions
 // against names of responders in a config file
-type InitializerTable map[string]func(jsonconfig.Obj) (fourthbot.RegisteringResponder, error)
+type InitializerTable map[string]Initializer
