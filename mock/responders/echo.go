1fced20faaf943e02015b54659f4031fb251b9ee
Simon Jefford <simon.jefford@fourth.com>
2017-06-19 18:18:03 +0100

    echo now responds "in channel"

--- mock/responders/echo.go
+++ mock/responders/echo.go
@@ -11,5 +11,6 @@ import (
 // Echo is a test responder intended for unit tests and adhoc testing
 // with the testbed.
 var Echo = fourthbot.ResponderFunc(func(ctx context.Context, c *fourthbot.Command, rw fourthbot.ResponseWriter) {
-	fmt.Fprintln(rw, strings.Join(c.Args, " "))
+	joined := strings.Join(c.Args, " ")
+	fmt.Fprintf(rw, "{\"response_type\": \"in_channel\",\"text\": \"%s\"}", joined)
 })
